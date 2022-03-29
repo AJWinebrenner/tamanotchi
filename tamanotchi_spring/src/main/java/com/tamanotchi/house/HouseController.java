fd8fe9af7709cd089b3891eddde0da69f6eb45d3
Nayan Gurung <nayangrg.se@gmail.com>
2022-03-29 11:55:19 +0000

    add selectAll and selectById to the houseController

---
+++ tamanotchi_spring/src/main/java/com/tamanotchi/house/HouseController.java
@@ -0,0 +1,27 @@
+package com.tamanotchi.house;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HouseController {
+
+    private HouseService houseService;
+
+    public HouseController(HouseService houseService) {
+        this.houseService = houseService;
+    }
+
+    @GetMapping(path="houses")
+    public List<House> getAllHouses (){
+        return houseService.selectAllHouses();
+    }
+
+    @GetMapping(path ="houses/{id}")
+    public House getHouseById(@PathVariable("id") Integer HouseId){
+        return houseService.selectHouseById(HouseId);
+    }
+}
