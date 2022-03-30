a40f07a8224791de4919d9c7dfd3f82f7f731e1c
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-03-30 16:54:09 +0100

    add colours and title

--- tamanotchi_react/src/pages/GamePage.js
+++ tamanotchi_react/src/pages/GamePage.js
@@ -56,6 +56,8 @@ const GamePage = ({petId}) => {
 
     return (
         <>
+            <div className="break"/>
+            <h1 className="break center-text">TAMANOTCHI</h1>
             <section id="banner" className="middle-flex break">
                 <div id="banner__stage" className="pixel-box center-text">
                     {currentStage}
