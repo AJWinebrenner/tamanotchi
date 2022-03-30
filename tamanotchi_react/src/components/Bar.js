7a8bc41300ce71a3eb994f8edb46320f86773224
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2022-03-30 14:37:18 +0100

    add responsive css vars for bars

--- tamanotchi_react/src/components/Bar.js
+++ tamanotchi_react/src/components/Bar.js
@@ -1,14 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 const Bar = ({value, max, className}) => {
 
     const root = document.documentElement;
 
-    const setCss = () => {
-        console.log(root);
-        let fraction = value/max;
-        root.style.setProperty(`--${className}-size`, fraction);
+    const calcFraction = (n, d) => {
+        let f = (n/d);
+        if (typeof f == 'number') {
+            return f;
+        } else {
+            return 0;
+        }
     }
+
+    const fraction = useMemo(() => calcFraction(value, max), [value,max]);
+
+    const setCss = () => {
+        console.log(value);
+        console.log(fraction);
+        root.style.setProperty(`--${className}-size`, fraction)
+    }
+
+    useEffect(setCss, [fraction])
+
 
     return(
         <div className={`pixel-box bar break ${className}`}></div>
